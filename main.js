8af8cec5f9907dce9cd4696155ddedce1641d926
AndreasPapaz <andreaspapaz@gmail.com>
2018-09-25 20:21:49 -0500

    wip; input numbers for tree

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+  console.log('on load');
+  const numberVal = document.getElementById('inputNumber');
+
+  document.addEventListener('click', function() {
+
+  })
+})
--- sketch.js
+++ sketch.js
@@ -2,22 +2,36 @@ let tree;
 
 function setup() {
   // noCanvas();
-  createCanvas(600, 400);
+  // createCanvas(600, 400);
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
   background(51);
   tree = new Tree();
-  tree.addNode(5);
-  tree.addNode(3);
-  tree.addNode(100);
-
-  console.log(tree);
-  tree.traverse();
-  for (var i = 0; i < 10; i++) {
-    tree.addNode(floor(random(0,100)))
-  }
-  console.log(tree);
-  tree.traverse();
+  // tree.addNode(5);
+  // tree.addNode(3);
+  // tree.addNode(100);
+  //
+  // console.log(tree);
+  // tree.traverse();
+  // for (var i = 0; i < 10; i++) {
+  //   tree.addNode(floor(random(0,100)))
+  // }
+  // console.log(tree);
+  // tree.traverse();
 }
 
 // function draw() {
 //
 // }
+
+window.onresize = function() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  canvas.size(w,h);
+  width = w;
+  height = h;
+};
+
+
+function addTreeNode() {
+  console.log(numInput);
+}
